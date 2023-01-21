69944d468b2123ae1d14c8a458849ca1839ea007
ayyıldız@software <sihlarsefa7@gmail.com>
2023-01-22 01:43:01 +0300

    Created IGenericRepsotitory and Function added

---
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        //IQueryable oldugu zaman ToList  ToListAsync gibi ifadelere ihtiyac duyar buda bize ekstra sorgu göndermemize yarar 
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        //Asycn kullanmamızın sebebi memoryde uzun süren işlemlerin olması
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task<T> AddAsync(T entity);
+        Task<T> AddRangeAsycn(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
