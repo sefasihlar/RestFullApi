626a32ecc2ff947bc1bea72897fdfa5573c363de
ayyıldız@software <sihlarsefa7@gmail.com>
2023-01-22 01:25:09 +0300

    Created Entity table

---
+++ NLayer.Core/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core
+{
+   //newlenmemesi için abstract olarak ayarladık
+   //Ortak olarak kullanıcak alan
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
